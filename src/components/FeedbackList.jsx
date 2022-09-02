3a81c2dff42f28ddbecd3e6e9348bf45aec0f2f2
el-maaroufi <74816091+elmaaroufi1@users.noreply.github.com>
2022-09-02 20:35:39 +0200

    new features

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,15 +1,25 @@
 import FeedbackItem from "./FeedbackItem"
+import {motion, AnimatePresence} from 'framer-motion'
 
 function FeedbackList({feedback, handleDelete}) {
     if (!feedback || feedback.length == 0){
      return <p>No feedback yet</p>
     }
+
   return (
     <div>
+      <AnimatePresence>
       {feedback.map((item)=>(
+        <motion.div 
+        key={item.id}
+        initial={{opacity: 0}}
+        animate={{opacity:1}}
+        exit={{opacity:0}}
+        >
         <FeedbackItem handleDelete={handleDelete} key={item.id} item={item} />
-        
+        </motion.div>
       ))}
+       </AnimatePresence>
     </div>
   )
 }
